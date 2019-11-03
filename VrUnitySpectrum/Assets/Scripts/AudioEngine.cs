79ca14dd9ac0756329c24f9abee8771307c3eb0c
Manuel-Philippe Hergenröder <mail@manuelhergenroeder.de>
2019-11-03 23:01:58 +0100

    added naudio lib, added fftw lib; first experimental fft draft

---
+++ VrUnitySpectrum/Assets/Scripts/AudioEngine.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NAudio;
+using UnityEngine;
+
+public class AudioEngine : MonoBehaviour
+{
+    public string filePath;
+    public static double[][] fftData;
+    private byte[] _importData;
+    public int importSampleRate;
+    public int importBitDepth;
+    public double[] fftFrequencies;
+
+    void Start()
+    {
+        filePath = @"D:\Temp\test.wav";
+
+        // Read in wav file
+        NAudio.Wave.WaveFileReader waveFileReader = new NAudio.Wave.WaveFileReader(filePath);
+        this._importData = new byte[waveFileReader.Length];
+        this.importSampleRate = waveFileReader.WaveFormat.SampleRate;
+        this.importBitDepth = waveFileReader.WaveFormat.BitsPerSample;
+
+        waveFileReader.Read(_importData, 0, (int)waveFileReader.Length);
+        waveFileReader.Close();
+
+        int numOfBytes = (int)waveFileReader.Length;
+        int numOfSamples = numOfBytes / (this.importBitDepth / 8);
+        double durationInSecs = numOfSamples / this.importSampleRate;
+        double chunkFactor = durationInSecs / 0.05; // 50ms
+        int chunkSize = (int)(numOfSamples / chunkFactor);
+        int numOfChunks = (int)System.Math.Ceiling(numOfSamples / chunkFactor);
+        int fftSize = (int)(this.importSampleRate * 0.0232199546485261); // magic number taken from 1024 for 44.100 Hz
+
+        Debug.Log("sampleRate: " + this.importSampleRate.ToString());
+        Debug.Log("numOfBytes: " + numOfBytes.ToString());
+        Debug.Log("numOfSamples: " + numOfSamples.ToString());
+        Debug.Log("durationInSecs: " + durationInSecs.ToString());
+        Debug.Log("chunkSize: " + chunkSize.ToString());
+        Debug.Log("numOfChunks: " + numOfChunks.ToString());
+        Debug.Log("fftSize: " + fftSize.ToString());
+
+        // Create map of frequencies for the all the bins
+        this.fftFrequencies = new double[fftSize / 2];
+        for (int i = 0; i < fftSize / 2; i++)
+            this.fftFrequencies[i] = (double)i / (fftSize / 2) * this.importSampleRate / 2;
+
+        double[][] input = new double[numOfChunks][];
+        double[][] result = new double[numOfChunks][];
+        fftData = new double[numOfChunks][];
+
+        Fft fft = new Fft(fftSize, Fft.WindowType.hamming, this.importSampleRate);
+
+        for (int i=0; i < numOfChunks; i++)
+        {
+            input[i] = new double[chunkSize * 2];
+            result[i] = new double[fftSize];
+            fftData[i] = new double[fftSize];
+
+            // Assume for now everything is 16 bits per sample
+            for (int j=0; j < chunkSize * 2; j += 2)
+            {
+                // abort if last chunk is smaller
+                if (i * j > numOfSamples)
+                    return;
+                input[i][j] = (double)System.BitConverter.ToInt16(this._importData, (i*chunkSize)+j);
+            }
+            fft.Run(input[i], result[i]);
+            fftData[i] = result[i];
+        }
+        fft.Dispose();
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ VrUnitySpectrum/Assets/Scripts/Fft.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using FFTWSharp;
+
+// FFT uses FFTW - see http://www.fftw.org/index.html
+// C# Wrapper based on the documentation of FFTWSharp - see https://github.com/tszalay/FFTWSharp
+
+public class Fft
+{
+    public enum WindowType
+    {
+        flat,
+        hann,
+        hamming,
+        blackman
+    };
+    private WindowType _winfunc;
+    private int _fftLength;
+    private double[] _fftWindow, _fout, _fin;
+    private System.IntPtr _pin, _pout, _fplan;
+    public double ampCf, pwrCf;
+
+    public Fft(int n, WindowType wt, int sampleRate)
+    {
+        this._fftLength = n;
+        this._pin = fftw.malloc(n * 2 * sizeof(double));
+        this._pout = fftw.malloc(n * 2 * sizeof(double));
+
+        this._fin = new double[n];
+        this._fout = new double[n];
+        this._fftWindow = new double[n];
+        
+        this._winfunc = wt;
+        this.MakeFftWindow();
+
+        for (int i = 0; i < n; i++)
+            this._fin[i] = 0.0;
+
+        this._fplan = fftw.dft_r2c_1d(n, this._pin, this._pout, fftw_flags.Estimate);
+    }
+
+    private void MakeFftWindow()
+    {
+        double alpha, a0, a1, a2;
+
+        switch (this._winfunc)
+        {
+            case WindowType.hann:
+                for (int i = 0; i < this._fftLength; i++)
+                    _fftWindow[i] = 0.5 - 0.5 * System.Math.Cos((double)i * 2 * System.Math.PI / (this._fftLength - 1));
+                this.ampCf = 6;
+                this.pwrCf = 4.3;
+                break;
+
+            case WindowType.hamming:
+                for (int i = 0; i < this._fftLength; i++)
+                    _fftWindow[i] = 0.54 - 0.46 * System.Math.Cos((double)i * 2 * System.Math.PI / (this._fftLength - 1));
+                this.ampCf = 5.35;
+                this.pwrCf = 4.0;
+                break;
+
+            case WindowType.blackman:
+                alpha = 0.16;
+                a0 = (1.0 - alpha) / 2.0;
+                a1 = 0.5;
+                a2 = alpha / 2;
+                for (int i = 0; i < this._fftLength; i++)
+                    _fftWindow[i] =
+                        a0
+                        - a1 * System.Math.Cos((double)i * 2 * System.Math.PI / (this._fftLength - 1))
+                        + a2 * System.Math.Cos((double)i * 4 * System.Math.PI / (this._fftLength - 1));
+                this.ampCf = 7.54;
+                this.pwrCf = 5.2;
+                break;
+
+            case WindowType.flat:
+            default:
+                for (int i = 0; i < this._fftLength; i++)
+                    _fftWindow[i] = 1.0;
+                this.ampCf = 0;
+                this.pwrCf = 0;
+                break;
+
+        }
+
+    }
+
+    private void DoFft(double[] outp)
+    {
+        double c = this._fftLength * this._fftLength;
+        double cf = 2.0 / 32767.0;
+
+        Marshal.Copy(this._fin, 0, this._pin, this._fftLength);
+        fftwf.execute(this._fplan);
+
+        Marshal.Copy(this._pout, this._fout, 0, this._fftLength);
+        for (int i = 0; i < this._fftLength / 2; i++)
+            outp[i] = cf * System.Math.Sqrt((this._fout[2 * i] * this._fout[2 * i] + this._fout[2 * i + 1] * this._fout[2 * i + 1]) / c);
+
+
+    }
+
+    public void Run(double[] inp, double[] outp)
+    {
+        for (int i = 0; i < this._fftLength; i++)
+            this._fin[i] = inp[i] * this._fftWindow[i];
+        this.DoFft(outp);
+    }
+    public void Dispose()
+    {
+        fftwf.free(this._pin);
+        fftwf.free(this._pout);
+        fftwf.destroy_plan(this._fplan);
+    }
+}
