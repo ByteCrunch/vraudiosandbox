9db0f60f3b73a4c7814e0c066ff225fae854ff3d
Manuel-Philippe Hergenröder <mail@manuelhergenroeder.de>
2019-11-05 14:23:00 +0100

    rewritten and refactored. Now it could even do the thing it should do.

--- VrUnitySpectrum/Assets/Scripts/AudioEngine.cs
+++ VrUnitySpectrum/Assets/Scripts/AudioEngine.cs
@@ -6,70 +6,120 @@ using UnityEngine;
 public class AudioEngine : MonoBehaviour
 {
     public string filePath;
-    public static double[][] fftData;
-    private byte[] _importData;
+    public double[][] fftData;
+    private double[] _importDataInSamples;
     public int importSampleRate;
     public int importBitDepth;
     public double[] fftFrequencies;
 
     void Start()
     {
+        this.LoadAudioData();
+        this.DoFft();
+    }
+    public void LoadAudioData()
+    {
+        //TODO this is still hardcoded for testing
         filePath = @"D:\Temp\test.wav";
 
-        // Read in wav file
+        // Read in wav file and convert into an array of samples
         NAudio.Wave.WaveFileReader waveFileReader = new NAudio.Wave.WaveFileReader(filePath);
-        this._importData = new byte[waveFileReader.Length];
+        int numOfBytes = (int)waveFileReader.Length;
         this.importSampleRate = waveFileReader.WaveFormat.SampleRate;
         this.importBitDepth = waveFileReader.WaveFormat.BitsPerSample;
 
-        waveFileReader.Read(_importData, 0, (int)waveFileReader.Length);
+        byte[] importDataInBytes = new byte[numOfBytes];
+        waveFileReader.Read(importDataInBytes, 0, numOfBytes);
         waveFileReader.Close();
 
-        int numOfBytes = (int)waveFileReader.Length;
+
+        // Convert into double array
+        // 8, 16, 24, 32 and 64 bits per sample are supported for now
         int numOfSamples = numOfBytes / (this.importBitDepth / 8);
-        double durationInSecs = numOfSamples / this.importSampleRate;
-        double chunkFactor = durationInSecs / 0.05; // 50ms
-        int chunkSize = (int)(numOfSamples / chunkFactor);
-        int numOfChunks = (int)System.Math.Ceiling(numOfSamples / chunkFactor);
-        int fftSize = (int)(this.importSampleRate * 0.0232199546485261); // magic number taken from 1024 for 44.100 Hz
+        this._importDataInSamples = new double[numOfSamples];
 
-        Debug.Log("sampleRate: " + this.importSampleRate.ToString());
-        Debug.Log("numOfBytes: " + numOfBytes.ToString());
-        Debug.Log("numOfSamples: " + numOfSamples.ToString());
-        Debug.Log("durationInSecs: " + durationInSecs.ToString());
-        Debug.Log("chunkSize: " + chunkSize.ToString());
-        Debug.Log("numOfChunks: " + numOfChunks.ToString());
-        Debug.Log("fftSize: " + fftSize.ToString());
-
-        // Create map of frequencies for the all the bins
-        this.fftFrequencies = new double[fftSize / 2];
-        for (int i = 0; i < fftSize / 2; i++)
-            this.fftFrequencies[i] = (double)i / (fftSize / 2) * this.importSampleRate / 2;
-
-        double[][] input = new double[numOfChunks][];
-        double[][] result = new double[numOfChunks][];
-        fftData = new double[numOfChunks][];
-
-        Fft fft = new Fft(fftSize, Fft.WindowType.hamming, this.importSampleRate);
-
-        for (int i=0; i < numOfChunks; i++)
+        switch (this.importBitDepth)
         {
-            input[i] = new double[chunkSize * 2];
-            result[i] = new double[fftSize];
-            fftData[i] = new double[fftSize];
-
-            // Assume for now everything is 16 bits per sample
-            for (int j=0; j < chunkSize * 2; j += 2)
-            {
-                // abort if last chunk is smaller
-                if (i * j > numOfSamples)
-                    return;
-                input[i][j] = (double)System.BitConverter.ToInt16(this._importData, (i*chunkSize)+j);
-            }
-            fft.Run(input[i], result[i]);
-            fftData[i] = result[i];
+            case 8:
+                for (int i = 0; i < numOfSamples; i++)
+                {
+                    this._importDataInSamples[i] = importDataInBytes[i];
+                }
+                break;
+            case 16:
+                for (int i = 0; i < numOfSamples; i++)
+                {
+                    this._importDataInSamples[i] = (double)System.BitConverter.ToInt16(importDataInBytes, i);
+                }
+                break;
+            case 24:
+                for (int i = 0; i < numOfSamples - 2; i++)
+                {
+                    this._importDataInSamples[i] = (double)(importDataInBytes[i] + (importDataInBytes[i + 1] << 8) + (importDataInBytes[i + 2]) << 16);
+                }
+                break;
+            case 32:
+                for (int i = 0; i < numOfSamples; i++)
+                {
+                    this._importDataInSamples[i] = (double)System.BitConverter.ToInt32(importDataInBytes, i);
+                }
+                break;
+            case 64:
+                for (int i = 0; i < numOfSamples; i++)
+                {
+                    this._importDataInSamples[i] = (double)System.BitConverter.ToInt64(importDataInBytes, i);
+                }
+                break;
+            default:
+                //TODO do some error handling here
+                break;
         }
-        fft.Dispose();
+    }
+
+    public void DoFft()
+    {
+        if (this._importDataInSamples != null && this._importDataInSamples.Length > 0)
+        {
+            // Calculate size of chunk that will be sent to FFT routine
+            int numOfSamples = this._importDataInSamples.Length;
+            double durationInSecs = numOfSamples / this.importSampleRate;
+            double chunkFactor = durationInSecs / 0.05; // 50ms per chunk
+            int chunkSize = (int)(numOfSamples / chunkFactor);
+            int numOfChunks = (int)System.Math.Ceiling(numOfSamples / chunkFactor);
+
+            int fftSize = (int)(this.importSampleRate * 0.0232199546485261); // magic number taken from 44.100 Hz / 1024
+
+            // Create map of frequencies for the bins
+            this.fftFrequencies = new double[fftSize / 2];
+            for (int i = 0; i < fftSize / 2; i++)
+                this.fftFrequencies[i] = (double)i / (fftSize / 2) * this.importSampleRate / 2;
+
+            // Do FFT per chunk
+            double[][] input = new double[numOfChunks][];
+            double[][] result = new double[numOfChunks][];
+
+            Fft fft = new Fft(fftSize, Fft.WindowType.hamming, this.importSampleRate);
+
+            for (int i = 0; i < numOfChunks; i++)
+            {
+                input[i] = new double[chunkSize];
+                result[i] = new double[fftSize];
+
+                for (int j = 0; j < chunkSize; j++)
+                {
+                    if (this._importDataInSamples.Length > (i * chunkSize) + j)
+                    {
+                        input[i][j] = this._importDataInSamples[(i * chunkSize) + j];
+                    } else
+                    {
+                        break; // for last chunk which might be smaller than chunkSize
+                    }
+                }
+                fft.Run(input[i], result[i]);
+            }
+            this.fftData = result;
+            fft.Dispose();
+        }
     }
 
     void Update()
