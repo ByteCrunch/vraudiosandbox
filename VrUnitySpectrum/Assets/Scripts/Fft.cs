b308cb6cb997317994d21272619b957b1821ec59
Manuel-Philippe Hergenröder <mail@manuelhergenroeder.de>
2020-01-17 12:23:35 +0100

    Removed _ due to naming convention

--- VrUnitySpectrum/Assets/Scripts/Fft.cs
+++ VrUnitySpectrum/Assets/Scripts/Fft.cs
@@ -15,47 +15,47 @@ public class Fft
         hamming,
         blackman
     };
-    private WindowType _winfunc;
-    private int _fftLength;
-    private double[] _fftWindow, _fout, _fin;
-    private System.IntPtr _pin, _pout, _fplan;
+    private WindowType winfunc;
+    private int fftLength;
+    private double[] fftWindow, fout, fin;
+    private System.IntPtr pin, pout, fplan;
     public double ampCf, pwrCf;
 
     public Fft(int n, WindowType wt, int sampleRate)
     {
-        this._fftLength = n;
-        this._pin = fftw.malloc(n * 2 * sizeof(double));
-        this._pout = fftw.malloc(n * 2 * sizeof(double));
+        this.fftLength = n;
+        this.pin = fftw.malloc(n * 2 * sizeof(double));
+        this.pout = fftw.malloc(n * 2 * sizeof(double));
 
-        this._fin = new double[n];
-        this._fout = new double[n];
-        this._fftWindow = new double[n];
+        this.fin = new double[n];
+        this.fout = new double[n];
+        this.fftWindow = new double[n];
         
-        this._winfunc = wt;
+        this.winfunc = wt;
         this.MakeFftWindow();
 
         for (int i = 0; i < n; i++)
-            this._fin[i] = 0.0;
+            this.fin[i] = 0.0;
 
-        this._fplan = fftw.dft_r2c_1d(n, this._pin, this._pout, fftw_flags.Estimate);
+        this.fplan = fftw.dft_r2c_1d(n, this.pin, this.pout, fftw_flags.Estimate);
     }
 
     private void MakeFftWindow()
     {
         double alpha, a0, a1, a2;
 
-        switch (this._winfunc)
+        switch (this.winfunc)
         {
             case WindowType.hann:
-                for (int i = 0; i < this._fftLength; i++)
-                    _fftWindow[i] = 0.5 - 0.5 * System.Math.Cos((double)i * 2 * System.Math.PI / (this._fftLength - 1));
+                for (int i = 0; i < this.fftLength; i++)
+                    fftWindow[i] = 0.5 - 0.5 * System.Math.Cos((double)i * 2 * System.Math.PI / (this.fftLength - 1));
                 this.ampCf = 6;
                 this.pwrCf = 4.3;
                 break;
 
             case WindowType.hamming:
-                for (int i = 0; i < this._fftLength; i++)
-                    _fftWindow[i] = 0.54 - 0.46 * System.Math.Cos((double)i * 2 * System.Math.PI / (this._fftLength - 1));
+                for (int i = 0; i < this.fftLength; i++)
+                    fftWindow[i] = 0.54 - 0.46 * System.Math.Cos((double)i * 2 * System.Math.PI / (this.fftLength - 1));
                 this.ampCf = 5.35;
                 this.pwrCf = 4.0;
                 break;
@@ -65,19 +65,19 @@ public class Fft
                 a0 = (1.0 - alpha) / 2.0;
                 a1 = 0.5;
                 a2 = alpha / 2;
-                for (int i = 0; i < this._fftLength; i++)
-                    _fftWindow[i] =
+                for (int i = 0; i < this.fftLength; i++)
+                    fftWindow[i] =
                         a0
-                        - a1 * System.Math.Cos((double)i * 2 * System.Math.PI / (this._fftLength - 1))
-                        + a2 * System.Math.Cos((double)i * 4 * System.Math.PI / (this._fftLength - 1));
+                        - a1 * System.Math.Cos((double)i * 2 * System.Math.PI / (this.fftLength - 1))
+                        + a2 * System.Math.Cos((double)i * 4 * System.Math.PI / (this.fftLength - 1));
                 this.ampCf = 7.54;
                 this.pwrCf = 5.2;
                 break;
 
             case WindowType.flat:
             default:
-                for (int i = 0; i < this._fftLength; i++)
-                    _fftWindow[i] = 1.0;
+                for (int i = 0; i < this.fftLength; i++)
+                    fftWindow[i] = 1.0;
                 this.ampCf = 0;
                 this.pwrCf = 0;
                 break;
@@ -88,29 +88,29 @@ public class Fft
 
     private void DoFft(double[] outp)
     {
-        double c = this._fftLength * this._fftLength;
+        double c = this.fftLength * this.fftLength;
         double cf = 2.0 / 32767.0;
 
-        Marshal.Copy(this._fin, 0, this._pin, this._fftLength);
-        fftwf.execute(this._fplan);
+        Marshal.Copy(this.fin, 0, this.pin, this.fftLength);
+        fftwf.execute(this.fplan);
 
-        Marshal.Copy(this._pout, this._fout, 0, this._fftLength);
-        for (int i = 0; i < this._fftLength / 2; i++)
-            outp[i] = cf * System.Math.Sqrt((this._fout[2 * i] * this._fout[2 * i] + this._fout[2 * i + 1] * this._fout[2 * i + 1]) / c);
+        Marshal.Copy(this.pout, this.fout, 0, this.fftLength);
+        for (int i = 0; i < this.fftLength / 2; i++)
+            outp[i] = cf * System.Math.Sqrt((this.fout[2 * i] * this.fout[2 * i] + this.fout[2 * i + 1] * this.fout[2 * i + 1]) / c);
 
 
     }
 
     public void Run(double[] inp, double[] outp)
     {
-        for (int i = 0; i < this._fftLength; i++)
-            this._fin[i] = inp[i] * this._fftWindow[i];
+        for (int i = 0; i < this.fftLength; i++)
+            this.fin[i] = inp[i] * this.fftWindow[i];
         this.DoFft(outp);
     }
     public void Dispose()
     {
-        fftwf.free(this._pin);
-        fftwf.free(this._pout);
-        fftwf.destroy_plan(this._fplan);
+        fftwf.free(this.pin);
+        fftwf.free(this.pout);
+        fftwf.destroy_plan(this.fplan);
     }
 }
