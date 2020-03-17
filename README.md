fde88a645a1ab52148351b83d97d7a505e6951a4
Manuel-Philippe Hergenröder <mail@manuelhergenroeder.de>
2020-03-17 09:39:28 +0100

    clean up and renaming after repo copy

--- README.md
+++ README.md
@@ -1,12 +1,8 @@
-# VrUnitySpectrum
+# VrAudioSandbox
 
-![Input mapping htc VIVE controllers](images/teleport2-web.png)
-
-VrUnitySpectrum is a student project for the Hochschule für Musik Karlsruhe to visualize FFT spectrum data in VR.
+VrAudioSandbox is a bachelor thesis project for the Hochschule für Musik Karlsruhe to manipulate audio data in VR.
 
 Author: Manuel-Philippe Hergenröder
-
-[Project documentation (german)](Visualisierung%20von%20Audio-Daten%20in%20VR%20-%20Dokumentation.pdf)
 
 ---
 
@@ -24,23 +20,4 @@ Author: Manuel-Philippe Hergenröder
 
 ### Getting started
 
-#### In Unity
-Just hit play.
-
-When stopped you can change the input audio file via the inspector of the "Audio"-GameObject in the "Audio Engine"-Component.
-
-#### With the standalone player
-After launching _VrUnitySpectrum.exe_ there will be a filechooser. You can select an audio file - see "Examples" to load one of the bundled files.
-
-#### Controls
-Use both of htc VIVE controllers for navigation and playback:
-
-![Input mapping htc VIVE controllers](images/vive-controller-web.png)
-
-Without a Head-Mounted-Display you can just hit Play in Unity and navigate with WASD and change the camera with your mouse (while holding down right-mouse-button)
-You can start/stop playback on the keyboard with _space_, press _backspace_ to rewind.
-
-You can exit the program with _ALT+F4_.
-
-### Misc
-Currently only mono audio files with 8, 16, 24, 32 or 64 bits per sample are supported. For now I've only tested WAV PCM files with sampling rates 44.1kHz, 48 kHz and 96 kHz. NAudio should also be able to read in: AIFF, MP3 (using ACM, DMO or MFT), G.711 mu-law and a-law, ADPCM, G.722, WMA, AAC, MP4 and more others with Media Foundation (untested).
+work in progress
---
+++ VrAudioSandbox/Assets/Scripts/Fft.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using FFTWSharp;
+
+// FFT uses FFTW - see http://www.fftw.org/index.html
+// C# Wrapper based on the documentation of FFTWSharp - see https://github.com/tszalay/FFTWSharp
+
+public class Fft
+{
+    public enum WindowType
+    {
+        flat,
+        hann,
+        hamming,
+        blackman
+    };
+    private WindowType winfunc;
+    private int fftLength;
+    private double[] fftWindow, dout, din;
+    private System.IntPtr pin, pout, fplan;
+    public double ampCf, pwrCf;
+
+    public Fft(int n, WindowType wt, int sampleRate)
+    {
+        this.fftLength = n;
+        this.pin = fftw.malloc(n * 2 * sizeof(double));
+        this.pout = fftw.malloc(n * 2 * sizeof(double));
+
+        this.din = new double[n];
+        this.dout = new double[n/2+1];
+        this.fftWindow = new double[n];
+        
+        this.winfunc = wt;
+        this.MakeFftWindow();
+
+        for (int i = 0; i < n; i++)
+            this.din[i] = 0.0;
+
+        this.fplan = fftw.dft_1d(n, this.pin, this.pout, fftw_direction.Forward, fftw_flags.Measure);
+    }
+
+    private void MakeFftWindow()
+    {
+        double alpha, a0, a1, a2;
+
+        switch (this.winfunc)
+        {
+            case WindowType.hann:
+                for (int i = 0; i < this.fftLength; i++)
+                    fftWindow[i] = 0.5 - 0.5 * System.Math.Cos((double)i * 2 * System.Math.PI / (this.fftLength - 1));
+                this.ampCf = 6;
+                this.pwrCf = 4.3;
+                break;
+
+            case WindowType.hamming:
+                for (int i = 0; i < this.fftLength; i++)
+                    fftWindow[i] = 0.54 - 0.46 * System.Math.Cos((double)i * 2 * System.Math.PI / (this.fftLength - 1));
+                this.ampCf = 5.35;
+                this.pwrCf = 4.0;
+                break;
+
+            case WindowType.blackman:
+                alpha = 0.16;
+                a0 = (1.0 - alpha) / 2.0;
+                a1 = 0.5;
+                a2 = alpha / 2;
+                for (int i = 0; i < this.fftLength; i++)
+                    fftWindow[i] =
+                        a0
+                        - a1 * System.Math.Cos((double)i * 2 * System.Math.PI / (this.fftLength - 1))
+                        + a2 * System.Math.Cos((double)i * 4 * System.Math.PI / (this.fftLength - 1));
+                this.ampCf = 7.54;
+                this.pwrCf = 5.2;
+                break;
+
+            case WindowType.flat:
+            default:
+                for (int i = 0; i < this.fftLength; i++)
+                    fftWindow[i] = 1.0;
+                this.ampCf = 0;
+                this.pwrCf = 0;
+                break;
+
+        }
+
+    }
+
+    private void DoFft(double[] outp)
+    {
+        double c = this.fftLength;
+        double cf = 2.0 / 32767.0;
+
+        Marshal.Copy(this.din, 0, this.pin, this.fftLength);
+        fftwf.execute(this.fplan);
+
+        Marshal.Copy(this.pout, this.dout, 0, this.fftLength / 2 + 1);
+        for (int i = 0; i < this.fftLength / 2; i++)
+            //outp[i] = cf * System.Math.Sqrt((this.dout[2 * i] * this.dout[2 * i] + this.dout[2 * i + 1] * this.dout[2 * i + 1]) / c);
+            outp[i] = cf * System.Math.Sqrt((this.dout[i] * this.dout[i] + this.dout[i + 1] * this.dout[i + 1]) / c);
+
+
+    }
+
+    public void Run(double[] inp, double[] outp)
+    {
+        for (int i = 0; i < this.fftLength; i++)
+            this.din[i] = inp[i] * this.fftWindow[i];
+        this.DoFft(outp);
+    }
+    public void Dispose()
+    {
+        fftwf.free(this.pin);
+        fftwf.free(this.pout);
+        fftwf.destroy_plan(this.fplan);
+    }
+}
