11aba210c33c68bf722b4a41ee450440ceafa02b
Manuel-Philippe Hergenröder <mail@manuelhergenroeder.de>
2020-05-22 15:29:53 +0200

    testing tool selection panel

---
+++ VrAudioSandbox/Assets/Scripts/LeftControllerActions.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class LeftControllerActions : MonoBehaviour
+{
+    public SteamVR_Action_Boolean LeftMenu;
+    public SteamVR_Input_Sources handType;
+    private GameObject leftHand;
+    private float xRotation;
+
+    public bool LeftMenuActive = false;
+
+    private AudioEngine audioEngine;
+    private SpectrumMeshGenerator spectrum;
+    private ToolHandler tool;
+    private GameObject ui;
+
+
+    private void Awake()
+    {
+        this.leftHand = GameObject.Find("LeftHand");
+    }
+
+    void Start()
+    {
+        this.audioEngine = GameObject.Find("Audio").GetComponent<AudioEngine>();
+        this.spectrum = GameObject.Find("SpectrumMesh").GetComponent<SpectrumMeshGenerator>();
+        this.tool = this.GetComponent<ToolHandler>();
+
+        this.ui = GameObject.Find("UI");
+        this.ui.SetActive(false);
+
+        this.LeftMenu.AddOnStateDownListener(this.LeftMenuActivate, handType);
+        this.LeftMenu.AddOnStateUpListener(this.LeftMenuDeactivate, handType);
+
+        this.xRotation = this.leftHand.transform.rotation.x;
+    }
+
+    private void LeftMenuActivate (SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    {
+        this.LeftMenuActive = true;
+        this.ui.SetActive(true);
+
+        // Freeze position
+        this.ui.transform.SetParent(GameObject.Find("Environment").transform);
+    }
+
+    private void LeftMenuDeactivate(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    {
+        this.LeftMenuActive = false;
+        this.ui.SetActive(false);
+        this.ui.transform.SetParent(GameObject.Find("HoverPoint").transform);
+    }
+
+    void Update()
+    {
+        // Handle tool selection menu
+        if (this.LeftMenuActive)
+        {
+            if (this.leftHand.transform.rotation.x - this.xRotation > 0.01)
+                this.tool.nextTool();
+            else if (this.leftHand.transform.rotation.x - this.xRotation < -0.01)
+                this.tool.prevTool();
+        }
+        if (Time.frameCount % 10 == 0)
+            this.xRotation = this.leftHand.transform.rotation.x;
+    }
+}
--- VrAudioSandbox/Assets/SteamVR_Input/SteamVR_Input_Actions.cs
+++ VrAudioSandbox/Assets/SteamVR_Input/SteamVR_Input_Actions.cs
@@ -45,6 +45,8 @@ namespace Valve.VR
         
         private static SteamVR_Action_Boolean p_default_Hold;
         
+        private static SteamVR_Action_Boolean p_default_LeftMenu;
+        
         private static SteamVR_Action_Vibration p_default_Haptic;
         
         private static SteamVR_Action_Vector2 p_platformer_Move;
@@ -173,6 +175,14 @@ namespace Valve.VR
             }
         }
         
+        public static SteamVR_Action_Boolean default_LeftMenu
+        {
+            get
+            {
+                return SteamVR_Actions.p_default_LeftMenu.GetCopy<SteamVR_Action_Boolean>();
+            }
+        }
+        
         public static SteamVR_Action_Vibration default_Haptic
         {
             get
@@ -254,6 +264,7 @@ namespace Valve.VR
                     SteamVR_Actions.default_ScaleMeshYDec,
                     SteamVR_Actions.default_ScaleMeshYInc,
                     SteamVR_Actions.default_Hold,
+                    SteamVR_Actions.default_LeftMenu,
                     SteamVR_Actions.default_Haptic,
                     SteamVR_Actions.platformer_Move,
                     SteamVR_Actions.platformer_Jump,
@@ -277,6 +288,7 @@ namespace Valve.VR
                     SteamVR_Actions.default_ScaleMeshYDec,
                     SteamVR_Actions.default_ScaleMeshYInc,
                     SteamVR_Actions.default_Hold,
+                    SteamVR_Actions.default_LeftMenu,
                     SteamVR_Actions.platformer_Move,
                     SteamVR_Actions.platformer_Jump,
                     SteamVR_Actions.buggy_Steering,
@@ -302,6 +314,7 @@ namespace Valve.VR
                     SteamVR_Actions.default_ScaleMeshYDec,
                     SteamVR_Actions.default_ScaleMeshYInc,
                     SteamVR_Actions.default_Hold,
+                    SteamVR_Actions.default_LeftMenu,
                     SteamVR_Actions.platformer_Jump,
                     SteamVR_Actions.buggy_Brake,
                     SteamVR_Actions.buggy_Reset};
@@ -327,6 +340,7 @@ namespace Valve.VR
                     SteamVR_Actions.default_ScaleMeshYDec,
                     SteamVR_Actions.default_ScaleMeshYInc,
                     SteamVR_Actions.default_Hold,
+                    SteamVR_Actions.default_LeftMenu,
                     SteamVR_Actions.platformer_Move,
                     SteamVR_Actions.platformer_Jump,
                     SteamVR_Actions.buggy_Steering,
@@ -351,6 +365,7 @@ namespace Valve.VR
             SteamVR_Actions.p_default_ScaleMeshYDec = ((SteamVR_Action_Boolean)(SteamVR_Action.Create<SteamVR_Action_Boolean>("/actions/default/in/ScaleMeshYDec")));
             SteamVR_Actions.p_default_ScaleMeshYInc = ((SteamVR_Action_Boolean)(SteamVR_Action.Create<SteamVR_Action_Boolean>("/actions/default/in/ScaleMeshYInc")));
             SteamVR_Actions.p_default_Hold = ((SteamVR_Action_Boolean)(SteamVR_Action.Create<SteamVR_Action_Boolean>("/actions/default/in/Hold")));
+            SteamVR_Actions.p_default_LeftMenu = ((SteamVR_Action_Boolean)(SteamVR_Action.Create<SteamVR_Action_Boolean>("/actions/default/in/LeftMenu")));
             SteamVR_Actions.p_default_Haptic = ((SteamVR_Action_Vibration)(SteamVR_Action.Create<SteamVR_Action_Vibration>("/actions/default/out/Haptic")));
             SteamVR_Actions.p_platformer_Move = ((SteamVR_Action_Vector2)(SteamVR_Action.Create<SteamVR_Action_Vector2>("/actions/platformer/in/Move")));
             SteamVR_Actions.p_platformer_Jump = ((SteamVR_Action_Boolean)(SteamVR_Action.Create<SteamVR_Action_Boolean>("/actions/platformer/in/Jump")));
